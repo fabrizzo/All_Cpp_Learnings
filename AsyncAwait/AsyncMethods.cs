f13240b8a224486a7ff5dc4acc7ae14829984d40
Dmitriy <fre-osa@mail.ru>
2022-02-20 03:04:44 +0700

    AsyncAwait

---
+++ AsyncAwait/AsyncMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class AsyncMethods
+    {
+//        Console.WriteLine($"Метод Main начал свою работу в потоке {Thread.CurrentThread.ManagedThreadId}.");
+//        try
+//        {               
+//               DoSomethingAsync('#');
+//            DoSomething('*');
+//        }
+   
+//        catch (Exception ) {}
+//        Console.WriteLine($"\n Метод Main закончил свою работу в потоке {Thread.CurrentThread.ManagedThreadId}.");
+//        Console.ReadLine();
+//}
+//        public static async void DoSomethingAsync(char p)
+//        {
+//            Console.WriteLine($"\n Метод DoSomethingAsync начал свою работу в потоке {Thread.CurrentThread.ManagedThreadId}.");
+//            await Task.Run(() => DoSomething(p));
+//            Console.WriteLine($"\n Метод DoSomethingAsync закончил свою работу свою работу в потоке {Thread.CurrentThread.ManagedThreadId}.");
+//        }
+//        public static void DoSomething(char s)
+//        {
+//            Console.WriteLine($"\n Метод DoSomething начал свою работу в потоке {Thread.CurrentThread.ManagedThreadId}.");
+//            Thread.Sleep(3000);
+//            for (int i = 0; i < 80; i++)
+//            {
+//                Console.Write($"{s}");
+//            }
+//            Console.WriteLine($"\n Метод DoSomething закончил свою работу свою работу в потоке {Thread.CurrentThread.ManagedThreadId}.");
+        }
+}
+
---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    public static class Programm
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Начало");
+            try
+            {
+                DandTAsync();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Нашел ошибку!" + e);
+            }
+            Console.WriteLine("Конец!");
+            Console.ReadLine();
+        }
+
+        private async static void DandTAsync()
+        {
+            await Task.Delay(100);
+            throw new InvalidOperationException();
+        }
+    }
+
+}
